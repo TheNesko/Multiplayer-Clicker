1fad8431021088f3f3acc4e1bf91e982cd792a2a
Czar0pl <skrimuofficial@gmail.com>
2023-06-12 19:45:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from random import randint as rnd
 import pygame as pg
 import sys
+
 pg.init()
 
 #-------Preferences--------    
@@ -11,6 +13,26 @@ game_title = "CLICKER MP"
 
 win = pg.display.set_mode((display_x, display_y))
 pg.display.set_caption(game_title)
+
+class Code:
+    def codeGen(max_len):
+        """
+            Wyznaczanie długości kodu dla naszych potrzeb w (max_len)
+            i na dole jego generowanie :) 
+        """
+        characters = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%&"
+        code=[]
+        for i in range(max_len):
+            rand_number = rnd(0,len(characters))
+            for char in characters[rand_number]:
+                continue
+            code.append(char)
+        return "Code: "+"".join(code)
+
+   # print(codeGen(10))  <- przykładowe wyświetlanie ¯\_(ツ)_/¯
+
+
+
 
 class Game:
     def __init__(self):
