ec41e3c1e5eb2d4b5a036617df1bb10a5c2fdc4a
Czar0pl <skrimuofficial@gmail.com>
2023-06-12 18:03:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import pygame as pg
+import sys
+pg.init()
+
+#-------Preferences--------    
+display_x = 700
+display_y = 500
+game_title = "CLICKER MP"
+#-------------------------
+
+
+win = pg.display.set_mode((display_x, display_y))
+pg.display.set_caption(game_title)
 
 class Game:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
     def run(self):
-        print("Hello World! maybe sex?.")
-        for x in range(10):
-            print(f"you sucked my cock {x+1} times")
-        pass
-
+        """
+            Zamykanie okna
+            
+        """
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+            pg.display.update()
 
 if __name__ == "__main__":
     game = Game()
