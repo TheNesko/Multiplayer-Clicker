e59360df04e4e1ea37acfce9ba26cdeca912e785
CursedIndel <neskoplayer@gmail.com>
2023-06-12 17:22:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+class Game:
+    def __init__(self) -> None:
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
