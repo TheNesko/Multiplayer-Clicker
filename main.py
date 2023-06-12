bdbf1bb1ffc1499196aba822027789104e9340c7
CursedIndel <neskoplayer@gmail.com>
2023-06-12 20:39:25 +0200

    Zmieniona struktura projektu

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from random import randint
 import pygame as pg
-import sys
+import socket
 
-pg.init()
 
 #-------Preferences--------    
-display_x = 700
-display_y = 500
-game_title = "CLICKER MP"
-#-------------------------
+DISPLAY_WIDTH = 700
+DISPLAY_HEIGHT = 500
+GAME_TITLE = "CLICKER MP"
+#--------------------------
 
-
-win = pg.display.set_mode((display_x, display_y))
-pg.display.set_caption(game_title)
 
 class Code:
     def generate(length):
@@ -28,23 +24,60 @@ class Code:
         return code
 
 
-print(Code.generate(10))
+class Server:
+    def __init__(self):
+        """
+        Ta klasa jest odpowiedzialna za logike multiplayer
+        host jest to kod generowany przez klase Code
+        """
+        self.server = socket.socket()
+        self.host = Code.generate(8)
+        self.port = 69420
 
 
 class Game:
     def __init__(self):
+        self.is_running = True
+
+        """
+        Inicjalizacja pygame
+        tworzenie okna
+        """
+        pg.init()
+        pg.display.set_caption(GAME_TITLE)
+        self.window = pg.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
+    def update(self):
+        """
+        logika gry
+        """
         pass
 
+    def render(self):
+        """
+        funkcje wyświetlające na ekranie
+        logika interfejsu
+        """
+        
+        #test
+        Rect = pg.Rect(DISPLAY_WIDTH/2,
+                       DISPLAY_HEIGHT/2,
+                       50,
+                       50)
+        pg.draw.rect(self.window, (250,20,30), Rect)
+        #----
+
+        pg.display.update()
+        self.window.fill((30, 30, 30))
+
     def run(self):
-        """
-            Zamykanie okna
-            
-        """
-        while True:
+        while self.is_running:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
-                    sys.exit()
-            pg.display.update()
+                    self.is_running = False
+            self.update()
+            self.render()
+            
 
 if __name__ == "__main__":
     game = Game()
